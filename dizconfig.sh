8299e58cf13e4ae45a9f8e55777517acba64e5a2
Henry Winn <henrywinn@henrywinn.com>
2018-12-19 02:23:17 +0300

    Adds install_algo command

--- dizconfig.sh
+++ dizconfig.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-function help () {
+help() {
     echo "dizconfig - A script to help me (henrywinn@henrywinn.com) configure my server to do stuff I want it to do"
     echo ""
     echo "    jazz init                  - include jasmine in the project";
@@ -8,15 +8,39 @@ function help () {
     echo "    jazz run                   - runs tests in browser";
 }
 
-function install_zsh () {
-    apt-get update
-    apt-get install zsh
-    chsh -s $(which zsh)
+install_zsh() {
+    sudo apt-get update
+    sudo apt-get install zsh
+    sudo chsh -s $(which zsh)
+}
+
+ALGO_URL="https://github.com/trailofbits/algo/archive/master.zip"
+
+install_algo() {
+    wget $ALGO_URL -P /tmp/algo
+    unzip /tmp/algo/algo-master -d ~/algo
+    
+    sudo apt-get update && sudo apt-get install \
+    build-essential \
+    libssl-dev \
+    libffi-dev \
+    python-dev \
+    python-pip \
+    python-setuptools \
+    python-virtualenv -y
+
+    python -m virtualenv --python=`which python2` env &&
+    source env/bin/activate &&
+    python -m pip install -U pip virtualenv &&
+    python -m pip install -r requirements.txt
 }
 
 case "$1" in
     install_zsh)
         install_zsh
+    ;;
+    install_algo)
+        install_algo
     ;;
     *)
         help
