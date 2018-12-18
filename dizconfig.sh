3137236ef73b719b296f8145d3ca0b475d491d59
Henry Winn <henrywinn@henrywinn.com>
2018-12-19 01:47:32 +0300

    First commit

---
+++ dizconfig.sh
@@ -0,0 +1,22 @@
+case "$1" in
+    install_zsh)
+        install_zsh
+    ;;
+    *)
+        help
+    ;;
+esac
+
+function help() {
+    echo "dizconfig - A script to help me (henrywinn@henrywinn.com) configure my server to do stuff I want it to do"
+    echo ""
+    echo "    jazz init                  - include jasmine in the project";
+    echo "    jazz create FunctionName   - creates ./src/FunctionName.js ./spec/FunctionNameSpec.js";
+    echo "    jazz run                   - runs tests in browser";
+}
+
+function install_zsh() {
+    apt-get update
+    apt-get install zsh
+    chsh -s $(which zsh)
+}
